e4f4dc24b10debb2539dc62ce9af0de1821ab3b4
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-09-29 19:18:26 +0000

    breedFetcher Tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 const request = require('request');
 
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      return callback("api inaccessable");
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback("Invalid Search");
+    }
+    return callback( null, (data["0"].description));
+  });
+};
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log("Error, api inaccessable");
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Breed not found");
-    return;
-  }
-  console.log(data["0"].description);
-  console.log(typeof data);
-});
+module.exports = {fetchBreedDescription};
